e97f91e0cd1b986f3afdb4b15f838e1820d4c24b
Aditya06kr <adityakr0679@gmail.com>
2024-07-06 02:25:37 +0530

    added googlebutton and navbar component

---
+++ frontend/src/components/GoogleButton.jsx
@@ -0,0 +1,40 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useContext } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { auth } from "../utils/firebase";
+import { UserContext } from "../UserContext";
+import { toast } from "react-toastify";
+
+const GoogleButton = () => {
+  const { setUserInfo } = useContext(UserContext);
+  function googleLogin() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then(async (user) => {
+        console.log(user);
+        setUserInfo({
+          uid: user.uid,
+          email: user.email,
+        });
+        toast.success("Registered Successfully");
+      })
+      .catch((err) => {
+        toast.error(err.code.split("/")[1]);
+      });
+  }
+  return (
+    <div
+      onClick={googleLogin}
+      className="flex items-center w-3/5 bg-blue-400 gap-1 p-1 rounded-sm cursor-pointer"
+    >
+      <span className="bg-white p-1">
+        <FcGoogle className="text-3xl" />
+      </span>
+      <span className="text-center w-full">
+        <span className="text-white text-lg ">Sign Up with Google</span>
+      </span>
+    </div>
+  );
+};
+
+export default GoogleButton;
