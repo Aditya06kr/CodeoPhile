4d5a01f5d9caee07e7ed965eca2250313c68cd6f
Aditya06kr <adityakr0679@gmail.com>
2024-07-06 02:29:59 +0530

    build layout

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { UserContext } from "./UserContext";
+import { auth } from "./utils/firebase";
 
 const Layout = () => {
+  const { setUserInfo } = useContext(UserContext);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserInfo({
+          uid: user.uid,
+          email: user.email,
+        });
+      } else setUserInfo(null);
+      console.log(user);
+    });
+  }, []);
   return (
     <>
-      <div className="bg-slate-500">Header</div>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
     </>
   );
 };
