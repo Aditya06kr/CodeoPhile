af1a3f9e84e25354deda9d318694935c4ab419f1
Aditya06kr <adityakr0679@gmail.com>
2024-07-06 02:30:30 +0530

    installed and use dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Slide}
+    />
     <BrowserRouter>
       <App />
     </BrowserRouter>
