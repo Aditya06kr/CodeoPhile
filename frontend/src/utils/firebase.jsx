3be1b928ff10afc1257a58675ee5a3b109475676
Aditya06kr <adityakr0679@gmail.com>
2024-07-06 02:27:00 +0530

    firebase setup

---
+++ frontend/src/utils/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWwma-rF88ggQ8xWF1NSHHw-9HygiO13k",
+  authDomain: "codeophile-69940.firebaseapp.com",
+  projectId: "codeophile-69940",
+  storageBucket: "codeophile-69940.appspot.com",
+  messagingSenderId: "1075231610994",
+  appId: "1:1075231610994:web:86f226c4fe0b3c6e5b0aeb"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth(app);
+export default app;
