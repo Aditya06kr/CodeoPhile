45bb0a133cf33a0831d3eccd2484aa280e45d66c
Aditya06kr <adityakr0679@gmail.com>
2024-07-08 03:06:53 +0530

    Clan Schema

---
+++ backend/models/Clan.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const clanSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [
+      {
+        user_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Clan = mongoose.model("Clan", clanSchema);
+
+export default Clan;
