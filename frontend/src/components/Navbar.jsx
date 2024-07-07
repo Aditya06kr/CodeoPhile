69499f37da22ae8faaf82df02c47eb93c44cd0c9
Aditya06kr <adityakr0679@gmail.com>
2024-07-08 00:12:07 +0530

    created Navigation menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 
 const Navbar = () => {
+  const {userInfo}=useContext(UserContext);
   return (
-    <div>Navbar</div>
+    <>
+      <div className='flex justify-between p-4 bg-color1 text-white'>
+        <div className='p-2'><Link to={"/"}>CodeoPhile</Link></div>
+        <div>
+          <div className='flex gap-10 p-2 bg-color1' >
+            <Link to={"/"}>Home</Link>
+            <Link to={"/dashboard"}>Dashboard</Link>
+            {userInfo ? <Link to={"/profile"}>Profile</Link> : <Link to={"/register"}>SignUp</Link>}
+            {userInfo ? <Link to={"/logout"}>Logout</Link> : <Link to={"/login"}>Login</Link>}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
