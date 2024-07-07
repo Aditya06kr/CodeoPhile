277b9340fc2b71df52dedde2c308212bda1e8155
Aditya06kr <adityakr0679@gmail.com>
2024-07-08 00:15:57 +0530

    Modified routings

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import connectDb from "./db/index.js";
 
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.json("Hello People");
-})
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_KEY,
+  })
+);
 
-app.listen(8080,(req,res)=>{
+try {
+  connectDb();
+  app.listen(8080, (req, res) => {
     console.log("Listening on port 8080");
-})
+  });
+} catch (err) {
+  console.log("Error in connecting to MongoDB:");
+}
+
+app.get("/api/v1", (req, res) => {
+  res.json("Hello People");
+});
+
+import userRouter from "./routes/UserRoute.js";
+app.use("/api/v1/user", userRouter);
+
+import codeforcesRouter from "./routes/CodeforcesRoute.js";
+app.use("/api/v1/codeforces",codeforcesRouter);
