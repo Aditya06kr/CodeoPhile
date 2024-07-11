6d77f1949d53df1b32f620e1cf41c64777e76706
Aditya06kr <adityakr0679@gmail.com>
2024-07-11 23:02:47 +0530

    added routes

--- backend/routes/CodeforcesRoute.js
+++ backend/routes/CodeforcesRoute.js
@@ -1,8 +1,13 @@
 import {Router} from "express";
-import { checkUser } from "../controllers/codeforcesController.js";
+import { addMember, allMembers, checkUser, createClan, standingsInfo, totalContests } from "../controllers/codeforcesController.js";
 
 const router=Router();
 
 router.route("/checkuser/:username").get(checkUser);
+router.route("/createclan").post(createClan);
+router.route("/members").post(addMember);
+router.route("/members/:clanName").get(allMembers);
+router.route("/totalcontests/:user").get(totalContests);
+router.route("/standings/:user").get(standingsInfo);
 
 export default router;
