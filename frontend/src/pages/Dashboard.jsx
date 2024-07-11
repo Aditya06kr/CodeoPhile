be03f749266c0c068f7fb1b305c5a50ab2386872
Aditya06kr <adityakr0679@gmail.com>
2024-07-11 22:59:58 +0530

    form clan

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,28 +2,64 @@ import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../UserContext";
 import axios from "axios";
 import { toast } from "react-toastify";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const [username, setUsername] = useState("");
   const [enabled, setEnabled] = useState(true);
+  const [clans, setClans] = useState([]);
+  const [clanName, setClanName] = useState("");
   const { userInfo, setUserInfo } = useContext(UserContext);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
+    console.log(userInfo?.email);
+    console.log(userInfo?.cfId);
     if (userInfo) {
       axios
         .get(`/user/dashboard/${userInfo.email}`)
         .then((res) => {
           if (res.data) {
+            console.log(res.data);
             setEnabled(false);
+            setUserInfo((prevUserInfo) => ({
+              ...prevUserInfo,
+              CfId: res.data.CfId,
+            }));
           } else setEnabled(true);
         })
         .catch((err) => {
           console.log(err);
         });
     }
-  }, [userInfo]);
+  }, [userInfo?.email]);
+
+  useEffect(() => {
+    if (userInfo && userInfo.cfId) {
+      // axios
+      //   .get(`/codeforces/Clans/${userInfo.cfId}`)
+      //   .then((res) => {
+      //     console.log(res.data);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+    }
+  }, [userInfo, enabled]);
 
   async function updateUsername() {
     try {
@@ -40,7 +76,7 @@ const Dashboard = () => {
           setEnabled(false);
           setUserInfo((prevUserInfo) => ({
             ...prevUserInfo,
-            cfId: username,
+            CfId: username,
           }));
           toast.success(res.data);
         })
@@ -53,11 +89,28 @@ const Dashboard = () => {
     }
   }
 
-  function handleClick() {}
+  function createClan() {
+    // console.log(userInfo);
+    try {
+      axios
+        .post("/codeforces/createclan", { clanName, createdBy: userInfo.CfId })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      navigate(`/clan?name=${clanName}`);
+      setClanName("");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <>
       <div>DashBoard</div>
-      {enabled && (
+      {enabled ? (
         <>
           <input
             type="text"
@@ -67,12 +120,52 @@ const Dashboard = () => {
           />
           <button onClick={updateUsername}>&rarr;</button>
         </>
-      )}
-      {!enabled && (
-        <div>
-          <h1>Groups</h1>
-          
-        </div>
+      ) : (
+        !enabled && (
+          <>
+            <div className="flex justify-between p-4">
+              <h1>Your Clans :- </h1>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline">Form a Clan</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Make your own Clan</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when you're
+                      done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Clan Name
+                      </Label>
+                      <Input
+                        id="name"
+                        className="col-span-3"
+                        value={clanName}
+                        onChange={(e) => setClanName(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        type="button"
+                        variant="secondary"
+                        onClick={createClan}
+                      >
+                        Done
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </>
+        )
       )}
     </>
   );
