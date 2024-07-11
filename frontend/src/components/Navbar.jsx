f758b2f81f4529d6861e2cf25a3fdd0c97a9c8f5
Aditya06kr <adityakr0679@gmail.com>
2024-07-12 03:14:43 +0530

    changed profile routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           <div className='flex gap-10 p-2 bg-color1' >
             <Link to={"/"}>Home</Link>
             <Link to={"/dashboard"}>Dashboard</Link>
-            {userInfo ? <Link to={"/profile"}>Profile</Link> : <Link to={"/register"}>SignUp</Link>}
+            {userInfo ? <Link to={`/profile/${userInfo.CfId}`}>Profile</Link> : <Link to={"/register"}>SignUp</Link>}
             {userInfo ? <Link to={"/logout"}>Logout</Link> : <Link to={"/login"}>Login</Link>}
           </div>
         </div>
